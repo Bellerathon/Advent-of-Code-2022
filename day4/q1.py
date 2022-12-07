d4513112f46e63ce3b514b9e9d7f51aefc957dde
Bellerathon <54466071+Bellerathon@users.noreply.github.com>
2022-12-08 01:10:43 +1100

    day 4 done, 2 stars

--- day4/q1.py
+++ day4/q1.py
@@ -51,8 +51,10 @@ for line in input:
   first = line.split(",")[0]
   second = line.split(",")[1]
 
+  # Check if first half is fully contained in the second half
   if int(first.split("-")[0]) >= int(second.split("-")[0]) and int(first.split("-")[1]) <= int(second.split("-")[1]):
     sum += 1
+  # Check if second half is fully contained in the first half
   elif int(second.split("-")[0]) >= int(first.split("-")[0]) and int(second.split("-")[1]) <= int(first.split("-")[1]):
     sum += 1
 
