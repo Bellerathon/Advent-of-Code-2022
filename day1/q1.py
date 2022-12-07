95c12f80530acea6977e65ae6e40d56569631d6d
Bellerathon <54466071+Bellerathon@users.noreply.github.com>
2022-12-08 01:10:28 +1100

    day 1 done, 2 stars

--- day1/q1.py
+++ day1/q1.py
@@ -47,9 +47,11 @@ elf = 0
 count = 0
 sum = 0
 for line in input:
+  # If line not empty then add value to group sum
   if line != "\n":
     sum += int(line.strip("\n"))
   else:
+    # New line detected thus check sum of current group and then reset it for next group
     if sum > max:
       max = sum
       elf = count
--- day1/q2.py
+++ day1/q2.py
@@ -20,5 +20,6 @@ for line in input:
     calories_array.append(sum)
     sum = 0
 
+# Sort array high -> low then get first 3 (the 3 highest values)
 calories_array.sort(reverse=True)
 print(calories_array[0] + calories_array[1] + calories_array[2])
