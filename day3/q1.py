61844b6a6500e1bde35b2e22e9e370ae86bfc103
Bellerathon <54466071+Bellerathon@users.noreply.github.com>
2022-12-08 01:10:36 +1100

    day 3 done, 2 stars

--- day3/q1.py
+++ day3/q1.py
@@ -40,10 +40,12 @@ sum = 0
 for line in input:
   line = line.strip("\n")
 
+  # Split line in half evenly
   first_half = line[:int(len(line)/2)]
   second_half = line[int(len(line)/2):]
 
   for char1 in first_half:
+    # Check if a char in first half appears in second half
     if char1 in second_half:
       if char1.isupper():
         sum += alpha.index(char1.lower()) + 27
